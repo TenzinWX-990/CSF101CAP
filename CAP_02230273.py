3776049417e1da791e650bd07be110868f3d0464
Tenzin Wangchuk <02230273.cst@rub.edu.bt>
2024-04-12 22:53:01 +0600

    CAP1

--- CAP_02230273.py
+++ CAP_02230273.py
@@ -12,11 +12,11 @@
 ################################
 
 def read_input():
-    my_file = open('input_3_cap1.txt','r')
+    my_file = open('CSF101CAP/input_3_cap1.txt','r')
     return my_file
 
 def calculate_score(point):
-    score = {'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7}
+    score = {'A X': 3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7}
     total_score = 0
     for line in point:
         key = line.strip()
